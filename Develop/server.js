18a63e40a8d3ac9e2eae24160030b33c3e6a932f
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-01-09 20:06:05 -0500

    added app.delete but nothing is working correctly

--- Develop/server.js
+++ Develop/server.js
@@ -16,11 +16,11 @@ app.use(express.static("public"));
 // HTML Routes  (shows you the page)
 module.export = function (app) {
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
+    res.sendFile(path.join(__dirname + "notes.html"));
 });
 // this is  a wildcard
 app.get("*", function(req,res){
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname + "index.html"));
   });
 }
 
@@ -35,7 +35,8 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
   app.get("/api/notes", function (req, res) {
     // fs.readFile(path.join(__dirname, "/db/db.json"))
-    res.sendFile(path.join(__dirname, "/db/db.json"))
+    // res.sendFile(path.join(__dirname, "/db/db.json"))
+    res.send(db);
 
 });
 
@@ -43,47 +44,65 @@ module.exports = function(app) {
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 // app.post("/api/notes", () => {
 //     // read file and add to it add property to object for that id  
 
 // });
     app.post("/api/notes", function(req, res) {
-         // read file
-    fs.readFile(path.join(__dirname, "/db/db.json"),(err, data) => {
+      let uniqueId = "";
+      for (let i = 0; i < newNote.length; i++) {
+      newNote[i].uniqueId = i + 1;
+      }
+      let newNote = {
+        title: req.body.title,
+        text :req.body.text,
+        "id": uniqueId
+      }
+      // read file
+    fs.readFile(path.join(__dirname + "/db/db.json"),(err, data) => {
     if (err) {
       console.log(err);
     }
-    let newNote = {
-      "title": req.body.title,
-      "text":req.body.text,
-      "id": uniqueId
-    }
-    for (let i = 0; i < newNote.length; i++) {
-    newNote[i].uniqueId = i + 1;
-    }
-    let notesArray = JSON.parse(data);
+    
+    
+    let allNotesArray = JSON.parse(data);
     // Append newReservation into reservationArray
-    notesArray.push(newNote);
+    allNotesArray.push(newNote);
 
-    fs.writeFile(path.join(__dirname, "/db/db.json"),JSON.stringify(notesArray), (err) => {
+    fs.writeFile(path.join(__dirname, "/db/db.json"),JSON.stringify(allNotesArray), (err) => {
     if (err) {
     console.log(err);
     }
+    console.log("Note has been written");
     });
     });
     
   
   // ---------------------------------------------------------------------------
   // Delete out notes
-    app.delete("/api/notes/:id", function(req, res) {
-      //code to delete note
+//     app.delete("/api/notes/:id", function(req, res) {
+//       //code to delete note
+// });
+    app.delete("/api/notes/:id", (req, res) => {
+
+      fs.readFile("./db/db.json", (err, data) => {
+        if (err) 
+        throw err;
+        
+        const newAllNotes = allNotes.map()
+
+        fs.writeFile("./db/db.json", JSON.stringify(newAllNotes), err => {
+          if (err) 
+          throw err;
+          // res.sendFile(path.join(__dirname, "/db/db.json"))
+          res.send(db);
+          console.log("You just deleted the note")
+        });
+      });
 });
 })}
 
 app.listen(PORT, () => {
     console.log(`Listening on Port: ${PORT}`);
 })
-
