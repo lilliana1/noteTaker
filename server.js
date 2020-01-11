82a85027729a981de917ecdf67f94671a47ab475
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-01-10 20:28:05 -0500

    app is running

--- server.js
+++ server.js
@@ -12,89 +12,50 @@ app.use(express.json());
 // For it to be able to read all my public files
 app.use(express.static("public"));
 
+
 // ----------------------------------API ROUTES--------------------------------------------------------------
 // API Routes (pass info)
-
-
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // ---------------------------------------------------------------------------
-  app.get("/api/notes", function (req, res) {
-    
-    fs.readFile("db/db.json", "utf8", (err, data)=>{
-      if(err) throw err;
-      res.json(JSON.parse(data))
-  
-    })
-
-    // res.sendFile(path.join(__dirname, "/db/db.json"))
-    // res.send(db);
-
-});
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // ---------------------------------------------------------------------------
-// app.post("/api/notes", () => {
-//     // read file and add to it add property to object for that id  
-
-// });
-    app.post("/api/notes", function(req, res) {
-      let uniqueId = "";
-      for (let i = 0; i < newNote.length; i++) {
-      newNote[i].uniqueId = i + 1;
-      }
-      let newNote = {
-        title: req.body.title,
-        text :req.body.text,
-        "id": uniqueId
-      }
-      // read file
-    fs.readFile(path.join(__dirname + "/db/db.json"),(err, data) => {
-    if (err) {
-      console.log(err);
-    }
-    
-    
-    let allNotesArray = JSON.parse(data);
-    // Append newReservation into reservationArray
-    allNotesArray.push(newNote);
-
-    fs.writeFile(path.join(__dirname, "/db/db.json"),JSON.stringify(allNotesArray), (err) => {
-    if (err) {
-    console.log(err);
-    }
-    console.log("Note has been written");
-    });
-    });
-    
-  
-  // ---------------------------------------------------------------------------
-  // Delete out notes
-//     app.delete("/api/notes/:id", function(req, res) {
-//       //code to delete note
-// });
-    app.delete("/api/notes/:id", (req, res) => {
-
-      fs.readFile("./db/db.json", (err, data) => {
-        if (err) 
-        throw err;
-        
-        const newAllNotes = allNotes.map()
-
-        fs.writeFile("./db/db.json", JSON.stringify(newAllNotes), err => {
-          if (err) 
-          throw err;
-          // res.sendFile(path.join(__dirname, "/db/db.json"))
-          res.send(db);
-          console.log("You just deleted the note")
-        });
-      });
-});
+// API GET Requests. Below code handles when users "visit" a page. In each of the below cases when a user visits a link
+app.get("/api/notes", function (req, res) {
+  fs.readFile("db/db.json", "utf8", (err, data)=> {
+    if(err) throw err;
+    res.json(JSON.parse(data))
 })
+});
+// API POST Requests. Below code handles when a user submits a form and thus submits data to the server. In each of the below cases, when a user submits form data (a JSON object) the JSON is pushed to the appropriate JavaScript array
+app.post("/api/notes", function(req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let newNote = req.body;
+  let uniqueID = (savedNotes.length).toString();
+  newNote.id = uniqueID;
+  savedNotes.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+  console.log("Note has been created", newNote);
+  res.json(savedNotes);
+})
+
+// Delete out notes
+
+app.delete("/api/notes/:id", function(req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteID = req.params.id;
+  let newID = 0;
+  console.log(`Note has been deleted`);
+  savedNotes = savedNotes.filter(currNote => {
+      return currNote.id != noteID;
+  })
+  
+  for (currNote of savedNotes) {
+      currNote.id = newID.toString();
+      newID++;
+  }
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+  res.json(savedNotes);
+})
+// ----------------------------------END API ROUTES--------------------------------------------------------------
+
 // ---------------------------------HTML ROUTES---------------------------------------------------------------
 
 // HTML Routes  (shows you the page)
@@ -107,9 +68,9 @@ app.get("*", function(req,res){
   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
+// ---------------------------------END OF HTML ROUTES---------------------------------------------------------------
 
-
-
+// waiting to be called
 app.listen(PORT, () => {
     console.log(`Listening on Port: ${PORT}`);
 })
